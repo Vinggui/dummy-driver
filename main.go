4302cfa4a467d908ed1725573cc9dcbc223677de
Vinicius G. Guimaraes <vinicius_galvao@msn.com>
2020-06-10 17:55:38 -0700

    first commit

---
+++ main.go
@@ -0,0 +1,201 @@
+// Package main implements a client for Greeter service.
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"time"
+
+	pb "github.com/Vinggui/iaut-drivers/go-driver/internal/driverpc"
+	"google.golang.org/grpc"
+)
+
+const (
+	address     = "localhost:50051"
+	defaultName = "world"
+)
+
+func createDevice() *pb.Device {
+	inputOne := &pb.IO{
+		Type:  pb.IO_BUTTON,
+		Code:  []byte("in3"),
+		Name:  "test input 3",
+		Value: "true",
+	}
+	outputFortyTwo := &pb.IO{
+		Type:   pb.IO_INT,
+		Code:   []byte("out21"),
+		Name:   "test output 21",
+		Value:  "42",
+		Ranges: []string{"0:50"},
+	}
+	device := &pb.Device{
+		Credential: &pb.Credential{
+			DriverID: []byte("PMbNixmFx87HsVHS6iAz"),
+			Token:    []byte("123"),
+		},
+		Code:    []byte("sensor"),
+		Address: []byte("127.0.0.1:8080"),
+		Name:    "Super sensor",
+		Icon:    "$ball",
+		Inputs:  []*pb.IO{inputOne},
+		Outputs: []*pb.IO{outputFortyTwo},
+	}
+	return device
+}
+
+func makeLog(ctx context.Context, log pb.LoggerClient) {
+	r, err := log.Error(ctx, &pb.LogRequest{
+		Credential: &pb.Credential{
+			DriverID: []byte("teste"),
+			Token:    []byte("123"),
+		},
+		Message: "UHUUUUl",
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Server Ans: %v\n", r)
+}
+
+func setDev(center pb.CenterAPIClient) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	device := createDevice()
+
+	r, err := center.SetDevice(ctx, device)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Server Ans: %v\n", r)
+}
+
+func report(center pb.CenterAPIClient) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	// Report Output
+	r, err := center.Report(ctx, &pb.ReportMessage{
+		Credential: &pb.Credential{
+			DriverID: []byte("PMbNixmFx87HsVHS6iAz"),
+			Token:    []byte("123"),
+		},
+		DeviceCode: []byte("sensor"),
+		OutputCode: []byte("out42"),
+		Value:      "23",
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Server Ans: %v\n", r)
+}
+
+func getDevs(center pb.CenterAPIClient) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	// Get Devices
+	r, err := center.GetDevices(ctx, &pb.Credential{
+		DriverID: []byte("PMbNixmFx87HsVHS6iAz"),
+		Token:    []byte("123"),
+	})
+	if err != nil {
+		panic(err)
+	}
+	for {
+		dev, err := r.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("%v.ListFeatures(_) = _, %v", center, err)
+		}
+		log.Println(dev)
+	}
+}
+
+func delDev(center pb.CenterAPIClient) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	device := &pb.Device{
+		Credential: &pb.Credential{
+			DriverID: []byte("PMbNixmFx87HsVHS6iAz"),
+			Token:    []byte("123"),
+		},
+		Code: []byte("sensor"),
+	}
+
+	// Delete Devices
+	r, err := center.DeleteDevice(ctx, device)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Server Ans: %v\n", r)
+}
+
+func confirm(center pb.CenterAPIClient, input *pb.InputCommand) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	conf := &pb.Confirmation{
+		Credential: &pb.Credential{
+			DriverID: []byte("PMbNixmFx87HsVHS6iAz"),
+			Token:    []byte("123"),
+		},
+		Input: input,
+	}
+
+	r, err := center.Confirm(ctx, conf)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Server Ans: %v\n", r)
+}
+
+func poll(center pb.CenterAPIClient) {
+	for {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+		defer cancel()
+
+		r, err := center.PollRequest(ctx, &pb.Credential{
+			DriverID: []byte("PMbNixmFx87HsVHS6iAz"),
+			Token:    []byte("123"),
+		})
+		if err != nil {
+			panic(err)
+		}
+		for {
+			command, err := r.Recv()
+			if err == io.EOF ||
+				ctx.Err() == context.DeadlineExceeded {
+				log.Println("retartarting")
+				break
+			}
+			if err != nil {
+				log.Fatalf("%v.ListFeatures(_) = _, %v", center, err)
+			}
+			confirm(center, command.Input)
+			log.Println(command)
+		}
+	}
+}
+
+func main() {
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	// log := pb.NewLoggerClient(conn)
+	center := pb.NewCenterAPIClient(conn)
+	// setDev(center)
+	poll(center)
+
+	// fmt.Printf("Greeting: %v\n", r)
+}
---
+++ makefile
@@ -0,0 +1,7 @@
+# This will install the necessary tools for this driver and allows to run
+.PHONY: install run
+install:
+	@go build main.go
+
+run:
+	@./main $(token)
